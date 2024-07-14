7b22947e587612d1b8c35eebc32f4dc5ec560b4e
GUNADEVELOPER <96541260+GUNADEVELOPER@users.noreply.github.com>
2024-07-14 12:57:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,15 +11,9 @@ resource "aws_instance" "example" {
   }
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = var.bucket_name
-  acl    = "private"
-}
+
 
 output "instance_id" {
   value = aws_instance.example.id
 }
 
-output "bucket_arn" {
-  value = aws_s3_bucket.example.arn
-}
